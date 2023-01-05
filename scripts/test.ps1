2dee2ebb67752e954be92afb7d3f0c5515148e5b
jens <jens@doellmann.com>
2023-01-05 12:35:34 +0100

    remove unused project

--- scripts/test.ps1
+++
@@ -1,6 +0,0 @@
-﻿$configFile = Join-Path $PSScriptRoot "nuget.integration-test.config"
-
-dotnet restore .\test\MinimalApiBuilder.Generator.NugetIntegrationTest --packages ./packages --configfile $configFile
-
-dotnet build .\test\MinimalApiBuilder.Generator.NugetIntegrationTest `
-  -c Release --packages ./packages --no-restore
