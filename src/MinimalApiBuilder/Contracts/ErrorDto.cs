f219d71ba7d9c8beb176dd9da6c83f59dff99989
jens <jens@doellmann.com>
2023-02-10 21:21:19 +0100

    commit missing files

---
+++ src/MinimalApiBuilder/Contracts/ErrorDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace MinimalApiBuilder;
+
+public class ErrorDto
+{
+    public required HttpStatusCode StatusCode { get; init; }
+
+    public required string Message { get; init; }
+
+    public required IEnumerable<string> Errors { get; init; }
+}
---
+++ src/MinimalApiBuilder/Endpoint/MinimalApiBuilderEndpoint.Send.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace MinimalApiBuilder;
+
+public abstract partial class MinimalApiBuilderEndpoint
+{
+    public Task SendErrorAsync(
+        HttpContext httpContext,
+        string message,
+        HttpStatusCode statusCode = HttpStatusCode.BadRequest,
+        CancellationToken cancellationToken = default)
+    {
+        httpContext.Response.StatusCode = (int)statusCode;
+        return httpContext.Response.WriteAsJsonAsync(
+            value: new ErrorDto()
+            {
+                StatusCode = statusCode,
+                Message = message,
+                Errors = ValidationErrors
+            },
+            cancellationToken: cancellationToken);
+    }
+}
