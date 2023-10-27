970b8b25529226b8966946f629cdf019f13f85f9
jens <jens@doellmann.com>
2023-10-27 13:11:02 +0200

    don't unnecessary double quotes

--- scripts/accept_snapshots.ps1
+++ scripts/accept_snapshots.ps1
@@ -1,4 +1,4 @@
 ﻿Get-ChildItem -Path $PSScriptRoot\..\tests\*\__snapshots__ -Recurse -Filter *.received.cs `
-| Select-Object @{ Name = "Path"; Expression = { $_.FullName } },
-@{ Name = "Destination"; Expression = { Join-Path $_.Directory ($_.Name -replace '.received.cs$', '.verified.cs') } } `
+| Select-Object @{ Name = 'Path'; Expression = { $_.FullName } },
+@{ Name = 'Destination'; Expression = { Join-Path $_.Directory ($_.Name -replace '.received.cs$', '.verified.cs') } } `
 | Move-Item -Force
