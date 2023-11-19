4b56656f3cb3ff5a730888be9436020cc565f488
jens <jens@doellmann.com>
2023-11-19 13:59:44 +0100

    add missing using

--- tests/Fixture.TestApi/Program.cs
+++ tests/Fixture.TestApi/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MinimalApiBuilder;
 using static MinimalApiBuilder.ConfigureEndpoints;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
