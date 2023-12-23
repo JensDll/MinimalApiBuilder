b46b66bdb490d415b4c7241bc0186d495e81ba08
jens <jens@doellmann.com>
2023-12-23 14:07:50 +0100

    fix formatting

--- tests/Fixture.TestApi/Common/LinearSearchContentTypeProvider.cs
+++ tests/Fixture.TestApi/Common/LinearSearchContentTypeProvider.cs
@@ -22,20 +22,13 @@ internal sealed class LinearSearchContentTypeProvider : IContentTypeProvider
         "image/x-icon"
     ];
 
-    public static LinearSearchContentTypeProvider Instance { get; } = new();
+    public static readonly LinearSearchContentTypeProvider Instance = new();
 
     public bool TryGetContentType(string subpath, [MaybeNullWhen(false)] out string contentType)
     {
         contentType = null;
-
         ReadOnlySpan<char> path = subpath.AsSpan();
         int index = path.LastIndexOf('.') + 1;
-
-        if (index == 0)
-        {
-            return false;
-        }
-
         ReadOnlySpan<char> extension = path[index..];
 
         for (int i = 0; i < s_values.Length; i += 2)
